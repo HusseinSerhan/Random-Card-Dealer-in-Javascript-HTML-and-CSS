2bb58126b49b819e0f505ac5c8ab671ff815d058
Hussein <husseinserhan3012@gmail.com>
2023-03-28 17:20:48 -0400

    Css

---
+++ app.js
@@ -0,0 +1,9 @@
+const suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
+
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'King', 'Queen', 'Jack', 'Ace']; 
+
+let randomSuit = Math.floor(Math.random() * suits.length); 
+
+let randomValue = Math.floor(Math.random() * values.length); 
+
+
