99ab47025a4e69e7515abbd5617461365106d48d
Hussein <husseinserhan3012@gmail.com>
2023-03-31 15:05:17 -0400

    Changes in CSS

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const suits = ["Hearts", "Spades", "Clubs", "Diamonds"];
-
+const suits = ["♥", "♠", "♣", "♦"];
 const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'King', 'Queen', 'Jack', 'Ace']; 
 
-let randomSuit = Math.floor(Math.random() * suits.length); 
+const randomSuit = Math.floor(Math.random() * suits.length); 
+const randomValue = Math.floor(Math.random() * values.length); 
 
-let randomValue = Math.floor(Math.random() * values.length); 
 
+// Replace elements 
+function replaceElement() {
+    const oldSuitTop = document.getElementsByClassName('suit-top');
+    const newSuitTop = document.createElement("span")
 
+    newSuitTop.classList.add("suit-top");
+    newSuitTop.innerHTML = suits[randomSuit];
+
+    oldSuitTop.replaceWith(newSuitTop);
+
+}
+
+window.onload = function(){
+    replaceElement();
+}
