41f45a6137e13d5a78a30fae104a1ed32f3885b0
Hussein <husseinserhan3012@gmail.com>
2023-04-02 15:33:03 -0400

    Fixed issues related to replacing the Suit. Refactored two functions into one

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 const suits = ["♥", "♠", "♣", "♦"];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'King', 'Queen', 'Jack', 'Ace']; 
+const values = 
+    ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+ './assets/vecteezy_king-playing-card-character-sticker_.jpg', 
+ './assets/vecteezy_queen-playing-card-character-sticker_.jpg', 
+ './assets/vecteezy_jack-playing-card-character-sticker_.jpg'];
 
-const randomSuit = Math.floor(Math.random() * suits.length); 
 const randomValue = Math.floor(Math.random() * values.length); 
 
 // Replace elements 
-function replaceSuitTop() {
-    const suits = ["♥", "♠", "♣", "♦"];
+function replaceSuit(){
+    const randomSuit = Math.floor(Math.random() * suits.length); 
+
     const firstSpan = document.querySelector(".card span:first-child"); 
     const newSpan = document.createElement("span"); 
-    newSpan.textContent = suits[randomSuit]; 
+    newSpan.textContent = suits[randomSuit];
     newSpan.classList.add("suit-top");
     firstSpan.replaceWith(newSpan)
-}
 
-function replaceSuitBottom() {
     const lastSpan = document.querySelector(".card span:last-child"); 
-    const newSpan = document.createElement("span"); 
-    newSpan.textContent = suits[randomSuit]; 
-    newSpan.classList.add("suit-bottom");
-    lastSpan.replaceWith(newSpan)
+    const newSpan2 = document.createElement("span"); 
+    newSpan2.textContent = suits[randomSuit];  
+    newSpan2.classList.add("suit-bottom");
+    lastSpan.replaceWith(newSpan2)
 }
 
-window.onload = function(){
-    replaceSuitTop();
-    replaceSuitBottom();
+// Replace Value
+function replaceValue() {
+    const card = document.querySelector(".card"); 
+    const valueElement = card.querySelector(".details .value");
+
+
+    const value = values[Math.floor(Math.random() * values.length)]; 
+
+    if (isNaN(value)) { 
+        const img = document.createElement("img");
+        img.src = value;
+        img.classList.add("value-img");
+        card.querySelector(".value").replaceWith(img);
+        randomValue = Math.floor(Math.random() * 8) + 9; 
+    } else {
+        const newValue = document.createElement("span"); 
+        newValue.textContent = value; 
+        newValue.classList.add("value");
+        card.querySelector(".details .value").replaceWith(newValue);
+    }
 }
+
+// Refresh Button
+document.getElementById("refresh-button").addEventListener("click", function() {
+    replaceSuit();
+    replaceValue();
+});
+
